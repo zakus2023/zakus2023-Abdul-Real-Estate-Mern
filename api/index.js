333ff22c791e91fe5194352a1875569a84648b54
abdul <idbsch2012@gmail.com>
2024-03-10 11:31:05 -0400

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,9 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+//to use the env variable
+dotenv.config();
+
+//declaring my application port  #2
 const port = 3000;
 
+//connecting to mongodb using environment variable  set in .env after installing dotenv in the root folder
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// creating my application #3
 const app = express();
 
+//creating my listeners #4
 app.listen(port, () => {
   console.log("App is listening on port: " + port);
 });
